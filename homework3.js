2522f555a90515ba19dd1b0a6b5bfc3df7aceeb1
rhndbnnr <rhndbnnr@gmail.com>
2023-11-05 13:49:57 -0700

    Finished

--- homework3.js
+++ homework3.js
@@ -1,1 +1,9 @@
-function newMember() {}
+function addMember() {
+  let name = memberForm.first.value + " " + memberForm.last.value;
+
+  let ul = document.getElementById("members");
+
+  let li = document.createElement("LI");
+  li.appendChild(document.createTextNode(name));
+  ul.appendChild(li);
+}
